87f70f37ee8bcef5ec7de3a99646a70d427a9fe3
Farras Hafizhudin <103520002+farrasjay@users.noreply.github.com>
2022-09-21 19:26:51 +0700

    "implement bonus from tugas 3"

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,10 +6,21 @@ from django.core import serializers
 # TODO: Create your views here.
 def show_watchlist(request):
     data_watchlist = watchList.objects.all()
+    movie_counter = 0
+
+    for movie in data_watchlist:
+        if (movie.watched):
+            movie_counter += 1
+    if (movie_counter < 10 - movie_counter):
+        show_message = "Wah, kamu masih sedikit menonton!"
+    else:
+        show_message = "Selamat, kamu sudah banyak menonton!"
+
     context = {
         'watchlist': data_watchlist,
         'nama': 'Farras Hafizhudin Indra Wijaya',
-        'npm' : '2106652682'
+        'npm' : '2106652682',
+        'message' : show_message
     }
     return render(request, "mywatchlist.html", context)
 
